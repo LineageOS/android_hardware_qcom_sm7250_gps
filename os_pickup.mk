17846f94e14b104579967e19e1f7bc28181b6a12
Alessandro Astone <ales.astone@gmail.com>
2021-01-11 23:51:49 +0100

    os_pickup: Define QC_OPEN_PATH
    All references in sm7250 repos point to the correct location
    if we simply define QC_OPEN_PATH as vendor/qcom/opensource
    
    Change-Id: Ibb580230212a08c73a251c88630217b192979eb4

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,6 +1,7 @@
 ifeq ($(TARGET_BOARD_PLATFORM),lito)
 ifneq ($(BUILD_WITHOUT_VENDOR),true)
 LOCAL_PATH := $(call my-dir)
+QC_OPEN_PATH := vendor/qcom/opensource
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
 endif
